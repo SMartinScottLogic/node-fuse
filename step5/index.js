72ad1893ea68dee7765a1aac4abec5baeca0d83f
Simon Martin <havvoric@gmail.com>
2017-10-22 14:37:10 +0100

    Example statfs added for dynamic FS.

--- step5/index.js
+++ step5/index.js
@@ -24,6 +24,21 @@ fuse.mount(mountPath, {
       return
     }
     cb(fuse.ENOENT)
+  },
+  statfs: function (path, cb) {
+      cb(0, {
+	    bsize: 1000000,
+	    frsize: 1000000,
+	    blocks: 1000000,
+	    bfree: 1000000,
+	    bavail: 1000000,
+	    files: 1000000,
+	    ffree: 1000000,
+	    favail: 1000000,
+	    fsid: 1000000,
+	    flag: 1000000,
+	    namemax: 1000000
+	  })
   }
 }, function (err) {
   if (err) throw err
