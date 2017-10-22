c5f0a1a5ae35131913836e2e03a26a08a1998d35
Simon Martin <havvoric@gmail.com>
2017-10-22 15:48:20 +0100

    Added stub server.

---
+++ step5/server.js
@@ -0,0 +1,33 @@
+var jsonApi = require("jsonapi-server");
+
+jsonApi.setConfig({
+  port: 16006,
+  graphiql: true,
+  swagger: {
+    title: "Example JSON:API Server",
+    version: "0.1.1",
+    description: "This is the API description block that shows up in the swagger.json",
+    contact: {
+      name: "API Contact",
+      email: "apicontact@holidayextras.com",
+      url: "docs.hapi.holidayextras.com"
+    },
+    license: {
+      name: "MIT",
+      url: "http://opensource.org/licenses/MIT"
+    }
+  }
+});
+
+jsonApi.define({
+  resource: "photos",
+  handlers: new jsonApi.MemoryHandler(),
+  attributes: {
+    title: jsonApi.Joi.string(),
+    url: jsonApi.Joi.string().uri(),
+    height: jsonApi.Joi.number().min(1).max(10000).precision(0),
+    width: jsonApi.Joi.number().min(1).max(10000).precision(0)
+  }
+});
+
+jsonApi.start();
